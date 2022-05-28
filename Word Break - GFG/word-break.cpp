5a15f29dc3b3e418f660635336702cd864def918
Vivek Kumar Giri <61221429+vkgiri11@users.noreply.github.com>
2022-05-28 07:21:34 +0530

    Added solution - LeetHub

---
+++ Word Break - GFG/word-break.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+// A : given string to search
+// B : vector of available strings
+
+class Solution
+{
+public:
+    bool contains(string word, vector<string> &B) {
+        for(int i = 0; i < B.size(); i++) {
+            if((word.compare(B[i])) == 0) return true;
+        }
+        
+        return false;
+    }
+    bool f(int start, string A, vector<string> &B, vector<int> dp) {
+        if(start == A.size()) return true;
+        
+        if(dp[start] != -1) dp[start];
+        
+        for(int end = start+1; end <= A.size(); end++) {
+            if(contains(A.substr(start, end-start), B) && f(end, A, B, dp)) 
+                return dp[start] = true;
+        }
+        
+        return dp[start] = false;
+    }
+    int wordBreak(string A, vector<string> &B) {
+        vector<int> dp(A.size(), -1);
+        return f(0, A, B, dp);
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> dict;
+        for(int i=0;i<n;i++){
+            string S;
+            cin>>S;
+            dict.push_back(S);
+        }
+        string line;
+        cin>>line;
+        Solution ob;
+        cout<<ob.wordBreak(line, dict)<<"\n";
+    }
+}
+  // } Driver Code Ends
