f69ddb04d0414c63797f4e1b4440907ac9559ab4
Vivek Kumar Giri <61221429+vkgiri11@users.noreply.github.com>
2022-07-10 15:42:32 +0530

    Time: 250 ms (52.70%), Space: 52.5 MB (15.25%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.cpp
+++ 935-knight-dialer/935-knight-dialer.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int knightDialer(int n) {
         int mod = 1e9+7;
+        // index 0 -> can move to 4&6 from 0
         vector<vector<int>> paths = {{4,6}, {6,8}, {7,9}, {4,8}, {3,9,0}, { }, 
                                      {1,7,0}, {2,6}, {1,3}, {2,4}};
         
@@ -14,6 +15,7 @@ public:
         for(int i = 2; i <= n; i++) 
             for(int j = 0; j < 10; j++) 
                 for(int p:paths[j]) 
+                    // cumulatively add from the previous knight move. For instance., F(2, 0) -> F(1,4) +  F(1,6) F(2,6) -> F(1,0) + F(1,1) + F(1,7)
                     dp[i][j] = (dp[i][j] + dp[i-1][p]) % mod;
                 
         int sum = 0;
